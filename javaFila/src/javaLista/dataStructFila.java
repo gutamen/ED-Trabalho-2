f9e65fe424a8beebfe28783884d1328fa4931aba
SystemAnalyticByBaker <gustavomacedo1366@hotmail.com>
2022-07-30 21:00:50 -0300

    V2

---
+++ javaFila/src/javaLista/dataStructFila.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaLista;
+
+/**
+ *
+ * @author gusta
+ */
+public class dataStructFila {
+    public dataStructFila next;
+    public int init;
+    public int end;
+    public int val;
+
+    public dataStructFila getNext() {
+        return next;
+    }
+
+    public void setNext(dataStructFila next) {
+        this.next = next;
+    }
+
+    public int getInit() {
+        return init;
+    }
+
+    public void setInit(int init) {
+        this.init = init;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+    
+}
